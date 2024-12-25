aaed43a52246dfb25e78875443b75f5c66458448
satanix0 <piyushbxr5@gmail.com>
2024-12-25 13:07:03 +0530

    L3.5 - DVC w Data v3

--- L3_DVC/dvc_demo.py
+++ L3_DVC/dvc_demo.py
@@ -12,9 +12,9 @@ df = pd.DataFrame(data)
 new_row_loc = {'Name': 'Dennis', 'Age': 20, 'City': 'Cityl'}
 df.loc[len(df.index)] = new_row_loc
 
-# # Adding new row to df for V3 of data
-# new_row_loc2 = {'Name': 'V3', 'Age': 30, 'City': 'Cityl'}
-# df.loc[len(df.index)] = new_row_loc2
+# Adding new row to df for V3 of data
+new_row_loc2 = {'Name': 'Riley', 'Age': 30, 'City': 'Cityl'}
+df.loc[len(df.index)] = new_row_loc2
 
 # Ensure the "data" directory exists at the root level
 data_dir = 'L3_DVC/data/'
